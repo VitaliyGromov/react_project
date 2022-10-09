8d159e3b9ce333938289e121a0aad15ce6dd3790
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2022-10-09 09:44:49 +0300

    add folder components delete unused files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Metallica', body: 'One of the greatest metal band ever'},
+        {id: 2, title: 'Megadeath', body: "Metallica's enemy"},
+        {id: 3, title: 'Сплин', body: 'Одна из самых крутых групп российского рока'}
+    ]);
+
+    function createPost(newPost) {
+        setPosts([...posts, newPost])
+    }
+
+    function removePosts(post) {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
+    return (
+        <div className="App">
+            <PostForm create={createPost}/>
+            <PostList remove={removePosts} posts={posts} title='Список различных групп'/>
+        </div>
+    );
 }
 
 export default App;
