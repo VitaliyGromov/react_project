661e24de34cdf66421aea9d8dc70575e4f7c902e
Vitaliy Gromov <gromov.vitaliy03@gmail.com>
2022-10-16 21:49:05 +0300

    educate useMemo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import './styles/App.css';
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/ui/select/MySelect";
+import MyInput from "./components/ui/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -9,6 +11,21 @@ function App() {
         {id: 2, title: 'Megadeath', body: "Metallica's enemy"},
         {id: 3, title: 'Сплин', body: 'Одна из самых крутых групп российского рока'}
     ]);
+
+    const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const sortedPosts = useMemo(() => {
+        console.log('Функция отработала');
+        if(selectedSort){
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        return posts;
+    }, [selectedSort, posts]);
+
+    const sortedAndSelectedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery));
+    }, [searchQuery, sortedPosts]);
 
     function createPost(newPost) {
         setPosts([...posts, newPost])
@@ -18,10 +35,35 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     }
 
+    function sortPosts(sort) {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    }
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <PostList remove={removePosts} posts={posts} title='Список различных групп'/>
+            <hr style={{margin: 20}}/>
+            <MyInput
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                placeholder="Что ищем?"
+            />
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка по"
+                options={[
+                    {value: 'title', name: 'Сортировка по названию'},
+                    {value: 'body', name: 'Сортировка по описанию'},
+                ]}
+            />
+            {sortedAndSelectedPosts.length
+                ?
+                <PostList remove={removePosts} posts={sortedAndSelectedPosts} title='Список различных групп'/>
+                : <h1 style={{textAlign: "center"}}>
+                    Посты не найдены!
+                </h1>
+            }
         </div>
     );
 }
